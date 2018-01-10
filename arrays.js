914cb742a404c57624fe268d2f000d4f1337b965
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-10 20:14:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ var chocolateBars = [ "snickers" , "hundred grand" , "kitkat" , "skittles" ]
  }
 
  function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.unshift();
+  return array.shift();
  }
 
 
